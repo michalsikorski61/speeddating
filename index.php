355f8d5b70c6cbdde6f727a1632307ce6c3e5e15
Michał Sikorski <michalsikorski61@gmail.com>
2024-06-06 09:48:19 +0200

    removed register link from index.php;

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
         <br>
         <input type="submit" value="Zaloguj">
     </form>
-    <a href="register.php">Rejestracja</a>
+    
     <br>
     <a href="admin_login.php">Logowanie Administratora</a>
     </div>
