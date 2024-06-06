98a6cfac5a40c94a8aa4704cd965de43481c6203
Michał Sikorski <michalsikorski61@gmail.com>
2024-06-06 08:07:16 +0200

    created class Database to taking care of dealing with db

---
+++ Database.php
@@ -0,0 +1,71 @@
+<?php
+class Database {
+    private $pdo;
+    private $error;
+    private $stmt;
+
+    public function __construct() {
+        $config = require 'dbconfig.php';
+        $db = $config['database'];
+        
+        $dsn = "mysql:host={$db['host']};dbname={$db['name']};charset=utf8mb4";
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+
+        try {
+            $this->pdo = new PDO($dsn, $db['username'], $db['password'], $options);
+        } catch (PDOException $e) {
+            $this->error = $e->getMessage();
+            echo $this->error;
+        }
+    }
+
+    public function query($sql) {
+        $this->stmt = $this->pdo->prepare($sql);
+    }
+
+    public function bind($param, $value, $type = null) {
+        if (is_null($type)) {
+            switch (true) {
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+        $this->stmt->bindValue($param, $value, $type);
+    }
+
+    public function execute() {
+        return $this->stmt->execute();
+    }
+
+    public function resultset() {
+        $this->execute();
+        return $this->stmt->fetchAll();
+    }
+
+    public function single() {
+        $this->execute();
+        return $this->stmt->fetch();
+    }
+
+    public function rowCount() {
+        return $this->stmt->rowCount();
+    }
+
+    public function lastInsertId() {
+        return $this->pdo->lastInsertId();
+    }
+}
+?>
